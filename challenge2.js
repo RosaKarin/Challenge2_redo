2c7f4713e27e5d8a81f72136fb636ca5b9d7ec8c
RosaKarin <50404764+RosaKarin@users.noreply.github.com>
2022-05-13 18:12:36 +0200

    update

--- challenge2.js
+++ challenge2.js
@@ -42,25 +42,37 @@ function updateTimeFrame(){
 		
 	if (h >= 6 && h < 8){ //Sunrise
         document.getElementBy('body').style.backgroundColor = ('#ffa47f') //sunrise
+        document.getElementById ('body').style.backgroundImage = "url('sunset.png')"; //sunrise
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundSize = "cover";
         document.getElementById('zon').style.background = 'radial-gradient(circle at top left, #ffd753, #e21b1b)'; //sunrise
         document.getElementById('keyframe').style.boxShadow = '0px 0px 5px #ffd753'; //sunrise
         
 	
     } else if (h >= 8 && h < 17){ 	//Day
         document.getElementById('body').style.background = 'linear-gradient(#60dbf9, #ffa47f)'; //day
+        document.getElementById ('body').style.backgroundImage = "url('Day.png')"; //day
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundSize = "cover";
         document.getElementById('zon').style.background = 'radial-gradient(circle at top left, #e21b1b. #ffd753)'; // day
         document.getElementById('keyframe').style.boxShadow = '0px 0px 5px white'; //day
         
     
-    } else if (h >= 15 && h <18) { //sundown
+    } else if (h >= 17 && h <18) { //sundown
         document.getElementById('body').style.backgroundColor = ('#e76243') //sundown
-        document.getElementById('zon').style.background = 'radial-gradient(circle at top left, #ffd753, #e21b1b)'; //sundown
-        document.getElementById('keyframe').style.boxShadow = '0px 0px 5px #ffd753'; //sundown
+        document.getElementById ('body').style.backgroundImage = "url('sunset.png')"; //sundown
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundSize = "cover";
+        // document.getElementById('zon').style.background = 'radial-gradient(circle at top left, #ffd753, #e21b1b)'; //sundown
+        // document.getElementById('keyframe').style.boxShadow = '0px 0px 5px #ffd753'; //sundown
 
 
 	} else if (h >= 18 && h < 24) { //Evening
         // background.classList.add('sunset');
         document.getElementById('body').style.background = 'linear-gradient(#0f0f57, #60dbf9)'; //night
+        document.getElementById ('body').style.backgroundImage = "url('Evening.png')"; //Night
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundSize = "cover";
         document.getElementById('zon').style.background = 'radial-gradient(circle at top left, white, grey)'; //night
         document.getElementById('keyframe').style.boxShadow = '0px 0px 5px white'; //night
 		
